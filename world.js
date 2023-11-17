3b57e491884838232e4f4cb3478ea527eeef1005
b13nmaN <blenmanjoel@gmail.com>
2023-11-17 10:25:42 -0400

    feat: Add functionality to get and display a country of countries from page

---
+++ world.js
@@ -0,0 +1,27 @@
+const lookupBtn = document.getElementById("lookup");
+const country = document.getElementById("country");
+const results = document.getElementById("result");
+
+lookupBtn.addEventListener("click", async () => {
+    try {
+        await lookupCountry();
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// search for a country
+async function lookupCountry() {
+    try {
+        const res = await fetch("world.php?country=" + country.value);
+        const data = await res.text(); 
+        console.log("This is the data: ", data);
+        displayResult(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResult(data) {
+    results.innerHTML = data;
+}
--- world.php
+++ world.php
@@ -1,17 +1,36 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// get a all countries
+
+
+
+
+// get query parameter
+$country = $_GET['country'];
+
+
+// get the country from the database
+$select = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+$select->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
+$select->execute();
+$results = $select->fetchAll(PDO::FETCH_ASSOC);
+
+  // echo '<ul>';
+  //   foreach ($results as $row) {
+  //       echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+  //   }
+  //   echo '</ul>';
+
 
 ?>
 <ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
+  <?php foreach ($results as $row): ?>
+    <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <?php endforeach; ?>
 </ul>
